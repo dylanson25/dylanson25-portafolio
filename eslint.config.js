6ed1ce5a30c6f49433a3dc60ea1d65c9788c5b3a
Dylanson25 <dylanson25@users.noreply.github.com>
2024-10-02 14:15:26 -0700

    [#12] - add astro eslint parser

--- eslint.config.js
+++ eslint.config.js
@@ -1,21 +1,31 @@
+import typescriptEslintParser from '@typescript-eslint/parser';
+import eslintPluginAstro from 'eslint-plugin-astro';
+import astroEslintParser from 'astro-eslint-parser';
+
 export default [
+	...eslintPluginAstro.configs.recommended,
 	{
-		parser: '@typescript-eslint/parser',
 		files: ['**/*.{js,jsx,ts,tsx,astro}'],
-		extends: [
-			'plugin:@typescript-eslint/recommended',
-			'plugin:astro/recommended',
-		],
-		overrides: [
-			{
-				files: ['*.astro'],
-				parser: 'astro-eslint-parser',
-				parserOptions: {
-					parser: '@typescript-eslint/parser',
-					extraFileExtensions: ['.astro'],
-				},
+		languageOptions: {
+			parser: typescriptEslintParser,
+			parserOptions: {
+				ecmaVersion: 'latest',
+				sourceType: 'module',
+				extraFileExtensions: ['.astro'],
 			},
-		],
+		},
+
+		rules: {},
+	},
+	{
+		files: ['**/*.astro'],
+		languageOptions: {
+			parser: astroEslintParser,
+			parserOptions: {
+				parser: '@typescript-eslint/parser',
+				extraFileExtensions: ['.astro'],
+			},
+		},
 		rules: {},
 	},
 ];
