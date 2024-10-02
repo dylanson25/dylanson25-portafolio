490a7d539b0648d837aa6459eb701e24c94829d5
Dylanson25 <dylanson25@users.noreply.github.com>
2024-10-02 07:51:24 -0700

    [#12] - Update Eslint config typescript

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,21 @@
-import eslintPluginAstro from "eslint-plugin-astro";
 export default [
-  ...eslintPluginAstro.configs.recommended,
-  {
-    files: ["**/*.{js,jsx,ts,tsx,astro}"],
-    rules: {},
-  },
+	{
+		parser: '@typescript-eslint/parser',
+		files: ['**/*.{js,jsx,ts,tsx,astro}'],
+		extends: [
+			'plugin:@typescript-eslint/recommended',
+			'plugin:astro/recommended',
+		],
+		overrides: [
+			{
+				files: ['*.astro'],
+				parser: 'astro-eslint-parser',
+				parserOptions: {
+					parser: '@typescript-eslint/parser',
+					extraFileExtensions: ['.astro'],
+				},
+			},
+		],
+		rules: {},
+	},
 ];
