3492baeeef31d56fa67453c434d4fe271a8b9971
dylanson25 <dylanson25@users.noreply.github.com>
2024-10-05 07:46:53 -0700

    [#29] - Integrate PWA vite inastro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,64 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
-
 import netlify from '@astrojs/netlify';
-
 import sitemap from '@astrojs/sitemap';
+
+import { VitePWA } from 'vite-plugin-pwa';
+import { seoConfig, manifest } from './src/utils/seoConfig';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://www.dylanson25.dev',
-    integrations: [tailwind(), sitemap()],
-    output: 'server',
-    adapter: netlify(),
+	build: {
+		inlineStylesheets: 'always',
+	},
+	compressHTML: true,
+	prefetch: true,
+	devToolbar: {
+		enabled: false,
+	},
+	site: seoConfig.baseURL,
+	integrations: [tailwind(), sitemap()],
+	output: 'server',
+	adapter: netlify(),
+	vite: {
+		build: {
+			cssMinify: 'lightningcss',
+		},
+		plugins: [
+			VitePWA({
+				registerType: 'autoUpdate',
+				manifest,
+				workbox: {
+					globDirectory: 'dist',
+					globPatterns: ['**/*.{html,js,css,woff,woff2,ttf,eot,ico}'],
+					runtimeCaching: [
+						{
+							urlPattern: /\.(?:png|jpg|jpeg|svg|gif|webp|avif)$/,
+							handler: 'CacheFirst',
+							options: {
+								cacheName: 'images',
+								expiration: {
+									maxEntries: 100,
+									maxAgeSeconds: 30 * 24 * 60 * 60,
+								},
+							},
+						},
+						{
+							urlPattern: /\.(?:woff|woff2|ttf|eot|ico)$/,
+							handler: 'CacheFirst',
+							options: {
+								cacheName: 'fonts',
+								expiration: {
+									maxEntries: 10,
+									maxAgeSeconds: 30 * 24 * 60 * 60,
+								},
+							},
+						},
+					],
+					navigateFallback: null,
+				},
+			}),
+		],
+	},
 });
