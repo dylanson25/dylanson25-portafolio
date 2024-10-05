93032c4250c88530a9d1d282a8049e0876a2a8fa
dylanson25 <dylanson25@users.noreply.github.com>
2024-10-05 16:18:24 -0700

    [#22] -  Add instagram icon

--- src/models/icon.model.ts
+++ src/models/icon.model.ts
@@ -1,11 +1,11 @@
 interface IconPathsModel {
-	name: 'github' | 'mail' | 'linkedin';
+	name: 'github' | 'mail' | 'linkedin' | 'instagram';
 	fill: string;
 	d: string;
 }
 
 export interface IconPropsModel {
-	name: 'github' | 'mail' | 'linkedin';
+	name: 'github' | 'mail' | 'linkedin' | 'instagram';
 	fill?: string;
 	fill_hover?: string;
 	classes?: string[];
@@ -16,4 +16,5 @@ export interface InconsModel {
 	github: IconPathsModel;
 	mail: IconPathsModel;
 	linkedin: IconPathsModel;
+	instagram: IconPathsModel;
 }
